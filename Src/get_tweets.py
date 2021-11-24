122b1103d305a1245fa9d00282f75f1a38b09681
justinbondurant <justin_bondurant@hotmail.com>
2021-11-24 17:05:43 -0500

    added comments about how to proceed with sampling

--- Src/get_tweets.py
+++ Src/get_tweets.py
@@ -11,6 +11,18 @@ def write_tweets(clean_tweets, tweets_path):
     with open(tweets_path, 'w') as fh:
         json.dump(clean_tweets, fh, indent=4)
 
+def write_locations(clean_tweets, locations_path):
+    user_locations = []
+    for clean_tweet_id, clean_tweet_data in clean_tweets.items():
+        location_string = clean_tweet_data['user_location']
+        if location_string == None:
+            location_string = ""
+        user_locations.append(location_string)
+    locations_file = open(locations_path, "w")
+    for location in user_locations:
+        locations_file.write(location + "\n")
+    locations_file.close()
+
 
 def build_query_string(query_word_list):
     query_list_hashtag = []
@@ -34,26 +46,41 @@ def build_query_string(query_word_list):
 #however we can possibly filter tweets by location after the search
 #in which case we want to get way more than 1000 tweets
 #this link might help https://stackoverflow.com/questions/36490085/how-to-get-twitter-users-location-with-tweepy
-def get_api_tweets(client, query_string):
+def get_api_tweets(client, query_string, start_time, end_time, num_tweets_collected):
     if len(query_string) > 512:
         raise ValueError('Query length too long')
     # dates can be changed, but must remain a span of 3 days
-    start_time = '2021-11-19T00:00:00Z'
-    end_time = '2021-11-22T00:00:00Z'
-    #starting with items = 110 to be gentle to api as I test the waters
-    num_tweets_collected = 110
 
-    search_results = tw.Paginator(client.search_recent_tweets, query = query_string, expansions = ["author_id", "geo.place_id"],
+    #starting with items = 110 to be gentle to api as I test the waters
+
+    search_results_list = None
+    if num_tweets_collected > 100:
+        search_results = tw.Paginator(client.search_recent_tweets, query = query_string, expansions = ["author_id", "geo.place_id"],
                                                  tweet_fields=[
                                                       "author_id", "created_at", "context_annotations",
                                                       "entities", "geo", "id", "lang", "public_metrics",
                                                       "possibly_sensitive", "referenced_tweets", "source", "text"
                                                   ],
+                                                  user_fields=["username"],
                                                   place_fields=[
                                                       "full_name", "country", "country_code", "geo", "name"
                                                   ],start_time = start_time, end_time = end_time, max_results=100).flatten(num_tweets_collected)
-
-    search_results_list = list(search_results)
+        search_results_list = list(search_results)
+    else:
+        #keep this count line for future purposes
+        abc = client.get_recent_tweets_count(query=query_string, granularity = "hour")
+        abc2 = 1
+        search_results = client.search_recent_tweets(query=query_string, expansions=["author_id", "geo.place_id"],
+                                                      tweet_fields=[
+                                                          "author_id", "created_at", "context_annotations",
+                                                          "entities", "geo", "id", "lang", "public_metrics",
+                                                          "possibly_sensitive", "referenced_tweets", "source", "text"
+                                                      ],
+                                                      user_fields=["username"],
+                                                      place_fields=[
+                                                          "full_name", "country", "country_code", "geo", "name"
+                                                      ], start_time=start_time, end_time=end_time, max_results=num_tweets_collected)
+        search_results_list = search_results.data
     return search_results_list
 
 
@@ -71,7 +98,7 @@ def get_twitter_client(credentials_dict):
 
 
 def get_query_word_list():
-    covid_words = ['covid', 'coronavirus']
+    covid_words = ['covid', 'coronavirus']#perhaps add pandemic
     vaccine_words = ['vaccine', 'vaccination', 'vax']
     brand_words = ['pfizer', 'moderna', 'janssen', '"johnson and johnson"', '"johnson & johnson"']
     query_word_list = covid_words + vaccine_words + brand_words
@@ -106,7 +133,7 @@ def clean_api_tweets(api_tweets, client):
         tweet_username = get_username(tweet_user, client)
         tweet_user_location = get_user_location(tweet_username, client)
         tweet_public_metrics = api_tweet.public_metrics
-        tweet_entities = api_tweet.entities
+        #tweet_entities = api_tweet.entities
 
         clean_tweets[tweet_id] = {}
         clean_tweets[tweet_id]['text'] = tweet_text
@@ -116,7 +143,7 @@ def clean_api_tweets(api_tweets, client):
         clean_tweets[tweet_id]['user_location'] = tweet_user_location
         clean_tweets[tweet_id]['id'] = tweet_id
         clean_tweets[tweet_id]['public_metrics'] = tweet_public_metrics
-        clean_tweets[tweet_id]['entities'] = tweet_entities
+        #clean_tweets[tweet_id]['entities'] = tweet_entities
     return clean_tweets
 
 
@@ -128,11 +155,31 @@ def main():
     credentials_dict = read_json(credentials_path)
     print(query_string)
     client = get_twitter_client(credentials_dict)
-    api_tweets = get_api_tweets(client, query_string)
+
+    #TODO make method that takes this 3 day period and splits it into 2minute periods and samples 30 tweets each time
+    #this has the disadantage perhaps of making tweets in off hours too (relatively) important, but you can't
+    #sample perfectly anyways
+    #this sample will possibly only collect tweets after 30 seconds of each minute, but that sample should be
+    #representative of the tweets between 0s and 30s of each minute
+
+    # honestly it would be amazing if I weighted my sample with the data from
+    # the method client.get_recent_tweets_count(query=query_string, granularity = "hour") since
+    #it would be imo a better sampling methodology which we can then write about in the report
+    #edit: that being said, the amount of canadian tweets will vary wildly, so you need to do like sample 10
+    #then if 1 of the tweets is canadian add it to tweets for timeframe, until you reach the weigthed
+    # quota of canadian tweets for that hour?
+    start_time = '2021-11-19T00:00:00Z'
+    end_time = '2021-11-22T00:00:00Z'
+    num_tweets_collected = 10
+    #TODO change this call
+    api_tweets = get_api_tweets(client, query_string, start_time, end_time, num_tweets_collected)
+
     tweets = clean_api_tweets(api_tweets, client)
     #TODO make this valid for multiple OS
-    tweets_path = '../data/tweets.txt' #might get changed to json later
+    tweets_path = '../data/tweets.json'
     write_tweets(tweets, tweets_path)
+    location_path = '../data/locations.txt'
+    write_locations(tweets, location_path)
 
     #in report, we'll probably have to mention that we manually remove tweets about say Johnson and Johnson, or Pfizer
     #that weren't about the vaccine
