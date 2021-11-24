7c005bf0c4f80357514e4586e186e45b1898be04
justinbondurant <justin_bondurant@hotmail.com>
2021-11-23 20:23:38 -0500

    finished writing methods of get tweets

--- Src/get_tweets.py
+++ Src/get_tweets.py
@@ -6,6 +6,13 @@ def read_json(credentials_path):
         credentials = json.load(fh)
         return credentials
 
+def write_tweets(tweet_results, tweets_path):
+    tweets_file = open(tweets_path, "w")
+    for tweet in tweet_results:
+        tweets_file.write(tweet + "\n")
+    tweets_file.close()
+
+
 def build_query_string(query_word_list):
     query_list_hashtag = []
     for query_word in query_word_list:
@@ -18,14 +25,32 @@ def build_query_string(query_word_list):
     for query_word in query_list_fixed:
         query_string += query_word
 
+
+
+
     #this doc explains following code parameters https://developer.twitter.com/en/docs/twitter-api/tweets/search/integrate/build-a-query
     query_string += ') lang:en -is:retweet -is:reply -is:nullcast'
     return query_string
 
+#mycourses mentions getting canadian tweets by timezone, but I don't think that's possible
+#we can't search for tweets by location without an academic account, which is for masters students and above
+#however we can possibly filter tweets by location after the search
+#in which case we want to get way more than 1000 tweets
+#this link might help https://stackoverflow.com/questions/36490085/how-to-get-twitter-users-location-with-tweepy
 def get_api_tweets(client, query_string):
     if len(query_string) > 512:
         raise ValueError('Query length too long')
-    search_results = client.search_recent_tweets(q=query_string,  count=100)
+
+    # dates can be changed, but must remain a span of 3 days
+    start_time = '2021-11-19T00:00:00Z'
+    end_time = '2021-11-22T00:00:00Z'
+
+    #old attempt without pagination or cursor,
+    #also am not sure which tutorial or stackexchange page made me do count=100
+    #search_results = client.search_recent_tweets(q=query_string,  count=100, start_time = start_time, end_time = end_time)
+    #starting with items = 150 to be gentle to api as I test the waters
+    num_tweets_collected = 150
+    search_results = tweepy.cursor(client.search_recent_tweets, q=query_string, start_time = start_time, end_time = end_time).items(num_tweets_collected)
     return search_results
 
 #taken from https://stackoverflow.com/questions/22469713/managing-tweepy-api-search
@@ -56,14 +81,19 @@ def main():
     credentials_path = '../data/credentials.json'
     credentials_dict = read_json(credentials_path)
     client = get_twitter_client(credentials_dict)
-    api_tweets = get_api_tweets(client)
+    query_word_list = get_query_word_list()
+    query_string = build_query_string(query_word_list)
+    tweets = get_api_tweets(client, query_string)
+
+    #TODO make this valid for multiple OS
+    tweets_path = '../data/tweets.txt' #might get changed to json later
+    write_tweets(tweets, tweets_path)
+
 
     #in report, we'll probably have to mention that we manually remove tweets about say Johnson and Johnson, or Pfizer
     #that weren't about the vaccine
     #this means we collect enough tweets (1500 should be more than enough) so we can have 1000 relevant tweets
-    query_word_list = get_query_word_list()
-
-
+    #also, if we can filter by location, then we might need to collect like 50000 tweets
 
 
 
