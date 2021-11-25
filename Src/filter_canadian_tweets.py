9f1cff156939dafc31b2d0ffe0f81a3389b0b3bf
justinbondurant <justin_bondurant@hotmail.com>
2021-11-25 10:14:41 -0500

    added to methods relevant to filtering canadian tweets

--- Src/filter_canadian_tweets.py
+++ Src/filter_canadian_tweets.py
@@ -5,32 +5,50 @@
 #things like newfoundland and labrador reduced to newfoundland
 #this list of approx 1500 tweets will need to be checked manually afterwards for validity of can locations
 #ie. just open tsv/csv in excel/pycharm and delete rows out of can
-def get_canadian_locations():
-    #pt for province/territory
-    pt1 = ['newfoundland', 'newfoundland and labrador', 'terre-neuve', 'terre neuve', 'terre-neuve et labrador', 'terre neuve et labrador', 'NL', 'TNL']
-    pt2 = ['nova scotia', 'nouvelle ecosse', 'nouvelle-ecosse', 'nouvelle écosse', 'nouvelle-écosse', 'NS', 'NE', 'NÉ']
-    #TODO fill till pt 13
-    pt3 = []
-    pt4 = []
-    pt5 = []
-    pt6 = []
-    pt7 = []
-    pt8 = []
-    pt9 = []
-    pt10 = []
-    pt11 = []
-    pt12 = []
-    pt13 = []
+#also for report, mention english can speakers aren't the best sample of can. you need both
+def get_canadian_locations_exact():
+    country = ['ca', 'can']
+    p1 = ['nl', 'tnl']
+    p2 = ['pei', 'ipe', 'pe']
+    p3 = ['ns', 'ne', 'né']
+    p4 = ['nb']
+    p5 = ['que', 'qc']
+    p6 = ['ont', 'on']
+    p7 = ['man' 'mb']
+    p8 = ['sask', 'sk']
+    p9 = ['alta', 'alb', 'ab']
+    p10 = ['bc', 'cb']
+    t1 = ['yt', 'yn']
+    t2 = ['nwt', 'tno', 'nt']
+    t3 = ['nvt', 'nt', 'nu']
+    #notice nt both in t2 and t3
 
-    country = ['canada', 'ca', 'can']
-
-    all_canadian_geo = pt1 + pt2 + pt3 + pt4 + pt5 + pt6 + pt7 + pt8 + pt9 + pt10
-    all_canadian_geo += pt11 + pt12 + pt13 + country;
+    all_canadian_geo = p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9 + p10
+    all_canadian_geo += t1 + t2 + t3 + country
     return all_canadian_geo
 
+#make sure - are replaced with spaces when comparing
+#hmm, some tweets were cut short like Grand Falls-Windsor, Newfoundl cause of
+#the 30 char limit
+def get_canadian_locations_sub():
+    country = ['canada']
+    p1 = ['newfoundland', 'labrador', 'terre neuve', 'labrador']
+    p2 = ['prince edward', 'prince édouard']
+    p3 = ['nova scotia', 'nouvelle ecosse', 'nouvelle écosse']
+    p4 = ['new brunswick', 'nouveau brunswick']
+    p5 = ['quebec', 'québec']
+    p6 = ['ontario']
+    p7 = ['manitoba']
+    p8 = ['saskatchewan']
+    p9 = ['alberta']
+    p10 = ['british columbia', 'colombie britannique']
+    t1 = ['yukon']
+    t2 = ['northwest territories', 'territoires du nord ouest']
+    t3 = ['nunavut']
 
-#then maybe replace non alph things with spaces from self rep. loc. and check if exact match  (i.e. for montreal,qc)
-
+    all_canadian_geo = p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9 + p10
+    all_canadian_geo += t1 + t2 + t3 + country
+    return all_canadian_geo
 
 def main():
     a=1
