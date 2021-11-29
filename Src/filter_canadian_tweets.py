fbd72545b9f5b64edd70b55e4d735b4d514f9112
justinbondurant <justin_bondurant@hotmail.com>
2021-11-28 21:20:37 -0500

    added treaties locations and turned letters with accents into normal letters

--- Src/filter_canadian_tweets.py
+++ Src/filter_canadian_tweets.py
@@ -1,4 +1,6 @@
 import re
+from unidecode import unidecode
+
 
 
 #province/territory abbreviations from https://www150.statcan.gc.ca/n1/pub/92-195-x/2011001/geo/prov/tbl/tbl8-eng.htm
@@ -23,34 +25,40 @@ def get_canadian_locations_exact():
     t3 = ['nvt', 'nt', 'nu']
     #notice nt both in t2 and t3, not important unless we weight sample by provinces, which is a bit too fancy
 
-    all_canadian_geo = p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9 + p10
-    all_canadian_geo += t1 + t2 + t3 + country
+    all_canadian_geo_list = p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9 + p10
+    all_canadian_geo_list += t1 + t2 + t3 + country
+    all_canadian_geo = set(all_canadian_geo_list)
     return all_canadian_geo
 
 def get_californian_cities_exact():
-    cali_cities = ['san', 'santa', 'rio']
+    cali_cities_list = ['san', 'santa', 'rio']
+    cali_cities = set(cali_cities_list)
     return cali_cities
 
 def get_californian_cities_sub():
     #I don't think i can put county here as of now. perhaps people say stuff like york county, ontario
-    popular = ['hollywood']
+    popular = ['hollywood', 'beverly hills', 'stanford', 'lake forest', 'newport beach', 'palm springs', 'malibu', 'laguna beach']
 
     cities_1_10 = ['los angeles', 'fresno', 'long beach', 'oakland', 'bakersfield', 'anaheim']
     cities_11_20 = ['stockton', 'riverside', 'irvine', 'chula vista', 'fremont', 'modesto']
     cities_21_26 = ['fontana', 'oxnard', 'huntington beach', 'glendale', 'elk grove', ]
     #skipped 27 since it's ontario california lol
     cities_28_38 = ['rancho cucamonga', 'oceanside', 'lancaster', 'garden grove', 'salinas', 'hayward', 'corona', 'sunnyvale', 'pomona', 'escondido']
-    cali_cities = popular + cities_1_10 + cities_11_20 + cities_21_26 + cities_28_38
+    cities_39_50 = ['roseville', 'torrance', 'fullerton', 'visalia', 'orange', 'pasadena', 'victorville', 'thousand oaks', 'simi valley', 'vallejo', 'concord']
+    cities_51_60 = ['berkeley', 'clovis', 'fairfield', 'antioch', 'carlsbad', 'downey', 'costa mesa', 'murrieta', 'ventura']
+    #skipped 54 which is richmond
+    cali_cities_list = popular + cities_1_10 + cities_11_20 + cities_21_26 + cities_28_38 + cities_39_50 + cities_51_60
+    cali_cities = set(cali_cities_list)
     return cali_cities
 
 #hmm, some tweets were cut short like Grand Falls-Windsor, Newfoundl cause of the 30 char limit
 def get_canadian_locations_sub():
     country = ['canada']
     p1 = ['newfoundland', 'labrador', 'terre neuve', 'labrador']
-    p2 = ['prince edward', 'prince édouard']
-    p3 = ['nova scotia', 'nouvelle ecosse', 'nouvelle écosse']
+    p2 = ['prince edward']
+    p3 = ['nova scotia', 'nouvelle ecosse']
     p4 = ['new brunswick', 'nouveau brunswick']
-    p5 = ['quebec', 'québec']
+    p5 = ['quebec']
     p6 = ['ontario']
     p7 = ['manitoba']
     p8 = ['saskatchewan']
@@ -60,14 +68,21 @@ def get_canadian_locations_sub():
     t2 = ['northwest territories', 'territoires du nord ouest']
     t3 = ['nunavut']
 
-    all_canadian_geo = p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9 + p10
-    all_canadian_geo += t1 + t2 + t3 + country
+    treaties = ['treaty 1', 'treaty 2', 'treaty 3', 'treaty 4', 'treaty 5', 'treaty 6', 'treaty 7', 'treaty 8', 'treaty 9', 'treaty 10', 'treaty 11']
+
+
+    all_canadian_geo_list = p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9 + p10
+    all_canadian_geo_list += t1 + t2 + t3 + country + treaties
+    all_canadian_geo = set(all_canadian_geo_list)
     return all_canadian_geo
 
 def is_tweet_canadian(tweet_loc):
     if tweet_loc == None:
         return False
     tweet_loc = tweet_loc.lower()
+    print(tweet_loc)
+    tweet_loc = unidecode(tweet_loc)
+    print(tweet_loc)
     clean_tweet_loc = re.sub("[^0-9a-zA-Z]+", " ", tweet_loc)#for things like terre-neuve
     tweet_loc_words = clean_tweet_loc.split()
     can_locs_sub = get_canadian_locations_sub()
