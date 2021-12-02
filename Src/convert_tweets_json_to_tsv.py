e64d63f2ccffab61f49cc293663607bdd07b216d
justinbondurant <justin_bondurant@hotmail.com>
2021-12-02 13:00:20 -0500

    added location check

--- Src/convert_tweets_json_to_tsv.py
+++ Src/convert_tweets_json_to_tsv.py
@@ -1,13 +1,18 @@
-
+from filter_canadian_tweets import is_tweet_canadian, is_user_can
 from get_tweets import read_json
 import pandas as pd
 
 def main():
 
-    tweets_path = '../data/sample50every8min/tweets/2021-11-25T12:00:00Z-to-2021-11-26T00:00:00Z.json'
-    csv_tweets_path = '../data/sample50every8min/csvTweets/2021-11-25T12:00:00Z-to-2021-11-26T00:00:00Z.csv'
+    tweets_path = '../data/mergedTweets/2021-11-26T00:00:00Z-to-2021-11-29T00:00:00Z.json'
+    csv_tweets_path = '../data/mergedCanTweets/2021-11-26T00:00:00Z-to-2021-11-29T00:00:00Z.csv'
+
+    #tweets_path = '../Data/sample70every8min/tweets/2021-11-27T10:00:00Z-to-2021-11-27T12:00:00Z.json'
+    #csv_tweets_path = '../data/mergedTweets/aaaaa.csv'
 
     tweets_json = read_json(tweets_path)
+
+    num_not_can = 0
 
     tweets_clean = []
     for tweet_id, tweet_content in tweets_json.items():
@@ -24,12 +29,19 @@ def main():
         tweets_content_clean['like_count'] = tweet_content['public_metrics']['like_count']
         tweets_content_clean['quote_count'] = tweet_content['public_metrics']['quote_count']
 
-        tweets_clean.append(tweets_content_clean)
+        #adding canadian check
+        tweet_is_canadian = is_tweet_canadian(tweets_content_clean['user_location'])
+        user_is_canadian = is_user_can(tweets_content_clean['username'])
+        if tweet_is_canadian and user_is_canadian:
+            tweets_clean.append(tweets_content_clean)
+        else:
+            num_not_can += 1
 
     df = pd.DataFrame(tweets_clean)
 
     df.to_csv(csv_tweets_path)
-    print(df)
+    #print(df)
+    print(num_not_can)
 
 
 if __name__ == '__main__':
